8c4811849c718771364223ed36b525821cbcb27e
agevavava <holhokgorgok@gmail.com>
2025-09-30 21:30:07 +0700

    menambahkan template

---
+++ fisika/rumus_baru/rumus.py
@@ -0,0 +1,14 @@
+"""
+Deskripsi mengenai rumus
+
+Args:
+    Data, argumen, atau input yang diperlukan untuk rumus tersebut.
+    Contoh: 
+        massa (float atau int)
+
+Returns:
+    Output dari hasil perhitungan.
+    Sertakan format dan type hints.
+    Contoh:
+        Returns: List berisi integer 
+"""
